75cc99e06d854a3faaff8452f5cf268d0e85a100
fiona <fionamiaojing@gmail.com>
2018-03-30 13:18:22 -0700

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,35 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return undefined;
+  } else if (typeof obj === 'boolean') {
+    return obj === true? 'true':'false';
+  } else if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (typeof obj === 'string') {
+    return '"' + obj.replace(/"/g, "'") + '"';
+  } else if (obj.constructor === Array) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      if (obj[i] === undefined) {
+        result.push(null)
+      } else {
+        result.push(stringifyJSON(obj[i]));
+      }
+    }
+    return '[' + result.join(',') + ']';
+  } else if (obj.constructor === Object) {
+    var result = [];
+    for (var key in obj) {
+      if (stringifyJSON(obj[key]) === undefined) {
+        continue;
+      } else {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + result.join(',') + '}';
+  }
 };
